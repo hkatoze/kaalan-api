2d89fe32506c21472bb0de7155eaf286ec3a70d7
hkatoze <harounakinda@outlook.fr>
2024-01-29 12:05:39 +0000

    fixing library books return

--- src/routes/getAllBooks.js
+++ src/routes/getAllBooks.js
@@ -24,34 +24,6 @@ module.exports = (app) => {
         res.json({ message, data: books });
       });
     }
-    if (req.query.libraryOf) {
-      const userId = req.query.libraryOf;
-      const libraryBooks = [];
-      User.findByPk(userId).then((user) => {
-        if (user === null) {
-          const message = `L'utilisateur demandé n'existe pas. Réessayer avec un autre identifiant.`;
-
-          return res.status(404).json({ message });
-        }
-
-        const libraryBooksId = user["libraryBooks"].split(";").filter(Boolean);
-
-        libraryBooksId.forEach((bookId) => {
-          const book = Book.findByPk(bookId).then((book) => {
-            if (book === null) {
-              const message = `Le livre  demandé n'existe pas. Réessayer avec un autre identifiant.`;
-
-              return res.status(404).json({ message });
-            }
-
-            return book;
-          });
-          libraryBooks.push(book);
-        });
-        const message = `La liste complète des livres a bien été reccupérée.`;
-        res.json({ message, data: libraryBooks });
-      });
-    }
 
     if (req.query.search) {
       const searchQuery = req.query.search;
@@ -67,6 +39,33 @@ module.exports = (app) => {
         res.json({ message, data: books });
       });
     }
+
+    if (req.query.library) {
+      const userId = req.query.library;
+
+      return User.findByPk(userId).then((user) => {
+        const libraryBooksId = user["libraryBooks"].split(";").filter(Boolean);
+        let libraryBooks = [];
+
+        // Utiliser Promise.all pour attendre que toutes les promesses soient résolues
+        const bookPromises = libraryBooksId.map((bookId) => {
+          return Book.findByPk(Number(bookId)).then((book) => {
+            if (book === null) {
+              const message = `Le livre demandé n'existe pas. Réessayer avec un autre identifiant.`;
+              return res.status(404).json({ message });
+            }
+            libraryBooks = [...libraryBooks, book];
+          });
+        });
+
+        // Utiliser Promise.all pour attendre que toutes les promesses soient résolues
+        return Promise.all(bookPromises).then(() => {
+          const message = `La librairie complète de l'utilisateur a bien été récupérée.`;
+          res.json({ message, data: libraryBooks });
+        });
+      });
+    }
+
     Book.findAll()
       .then((books) => {
         const message = `La liste complète des livres a bien été reccupérée.`;
